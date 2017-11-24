2f8883ebe7f470d136061e7a1c631414eb91fad6
Lázaro Lima dos Santos <lazaro.lima@gmail.com>
2017-11-24 16:39:12 -0200

    Cria projeto

---
+++ SomDePlay/SomDePlayTests/SomDePlayTests.swift
@@ -0,0 +1,36 @@
+//
+//  SomDePlayTests.swift
+//  SomDePlayTests
+//
+//  Created by Lazaro Lima on 24/11/2017.
+//  Copyright © 2017 Lazaro Lima. All rights reserved.
+//
+
+import XCTest
+@testable import SomDePlay
+
+class SomDePlayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
